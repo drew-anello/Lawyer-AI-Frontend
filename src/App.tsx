73c012c1b3f0cf32626e4d7c305e77f060cc911c
drew-anello <drew.anello@gmail.com>
2025-06-24 22:04:17 -0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<DemandGeneratorPage />} />
-        {/* Future pages can be added here */}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
